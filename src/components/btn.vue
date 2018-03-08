4cc23c894531edf010651bf90597342089285afe
zhethon <kgb00128@gmail.com>
2018-03-08 08:20:29 +0800

    build components

---
+++ src/components/btn.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  div(:class="[btnClass,cname]")
+    slot
+</template>
+
+<script>
+  export default {
+    props: {
+      cname: {
+        type: String,
+        default: "",
+      },
+    },
+    data () {
+      return {
+        btnClass: "btn",
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/element.scss";
+  .btn {
+    @include btn;
+  }
+</style>
---
+++ src/components/panel.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  section(:class="[panelClass,cname]")
+    h4 {{ title }}
+    slot
+</template>
+
+<script>
+  export default {
+    props: {
+      cname: {
+        type: String,
+        default: "",
+      },
+      title: {
+        type: String,
+        default: "",
+      },
+    },
+    data () {
+      return {
+        panelClass: "panel",
+      }
+    }
+  }
+</script>
