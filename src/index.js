fb011f9955fcbb76cc7bbe6d84e6093053823ee8
Pitt Wu <kgb00128@gmail.com>
2021-12-13 15:50:12 +0800

    feat: connect mongodb

--- src/index.js
+++ src/index.js
@@ -7,6 +7,14 @@ const morgan = require('morgan');
 
 dotenv.config();
 
+mongoose.connect(
+  process.env.MONGODB_URL,
+  { useNewUrParser: true, useUnifiedTopology: true },
+  () => {
+    console.log(`Connected to MongoDB`);
+  }
+);
+
 const PORT = 3000;
 
 app.listen(PORT, () => {
